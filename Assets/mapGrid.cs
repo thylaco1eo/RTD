144acd8d92b45e8fa5304edf05f86aa0e1d8c0bb
thylaco1eo <frank.tancf@gmail.com>
2022-07-23 10:08:56 +0200

    map

--- Assets/mapGrid.cs
+++ Assets/mapGrid.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,27 +16,43 @@ public class mapGrid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int type;
+        string s;
         cells = new mapcell[height * width];
         for (int i = 0, count = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                CreateCell(j,i,count++);
+                if (i == j)
+                {
+                    type = 1;
+                }
+                else
+                {
+                    type = 0;
+                }
+                CreateCell(j,i,count++,type);
+                s = cells[count - 1].SavetoString();
+                Console.WriteLine(s);
+                File.WriteAllText("./Assets/data/map.json", s);
             }
         }
+        
     }
 
-    void CreateCell(int x, int y, int count)
+    void CreateCell(int x, int y, int count,int type)
     {
         Vector3 position;
-        position.x = x * 10;
-        position.y = y * 10;
+        position.x = x;
+        position.y = y;
         position.z = 0;
         cells[count] = Instantiate<mapcell>(cellPrefab);
         cells[count].transform.SetParent(transform,false);
+        cells[count].setCelltype(type);
         cells[count].transform.localPosition = position;
-        cells[count].transform.localScale = new Vector3(10, 10, 1);
+        cells[count].transform.localScale = new Vector3(1, 1, 1);
     }
+
     // Update is called once per frame
     void Update()
     {
--- Assets/mapcell.cs
+++ Assets/mapcell.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class mapcell : MonoBehaviour
 {
+    private int Celltype;
     // Start is called before the first frame update
+
+    public void setCelltype(int type)
+    {
+        this.Celltype = type;
+    }
+
+    public string SavetoString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+    
     void Start()
     {
         
